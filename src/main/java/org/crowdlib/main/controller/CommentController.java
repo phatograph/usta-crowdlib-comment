ec7983caf97508d26a622423cf160c5b5f265e16
Phat Wangrungarun <phatograph@gmail.com>
2015-11-22 15:50:13 +0000

    Add comment to comment

--- src/main/java/org/crowdlib/main/controller/CommentController.java
+++ src/main/java/org/crowdlib/main/controller/CommentController.java
@@ -1,12 +1,19 @@
 package org.crowdlib.main.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.crowdlib.model.Comment;
 import org.crowdlib.model.inmem.InMemComment;
+import org.crowdlib.model.inmem.InMemItem;
+import org.crowdlib.model.inmem.InMemUser;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Path("/comments")
 public class CommentController {
@@ -18,6 +25,24 @@ public class CommentController {
     public Response getComment(@PathParam("id") int id, @QueryParam("from") int from, @QueryParam("limit") int limit) {
         Comment c = InMemComment.get(id);
         return Response.ok(g.toJson(c.getComments(from, limit))).build();
+    }
+
+    @POST
+    @Path("{id}/comments")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addComment(@PathParam("id") int id, InputStream is) {
+        JsonParser parser = new JsonParser();
+        JsonElement json = parser.parse(new InputStreamReader(is));
+        JsonObject jo = json.getAsJsonObject();
+
+        Comment c = new InMemComment(
+                jo.get("content").getAsString(),
+                InMemUser.getCurrentUser(),
+                InMemComment.get(id)
+        );
+
+        return Response.ok(g.toJson(c)).build();
     }
 }
 
